c8890d0add0f625bae6096ff1bb4023f597e038e
brad <ymknkcs@163.com>
2017-08-06 17:59:18 +0800

    test

--- 建表用.sql
+++ 建表用.sql
@@ -3,3 +3,6 @@ create table dc_wallet(wid int primary key auto_increment,uid int not null refer
 create table dc_trade(tid int primary key auto_increment,wid int not null references dc_wallet(wid),volume int not null,tradetime time,memo varchar(50));
 #创建视图
 create view dc_user_wallet as select a.uid as uid, a.itcode as itcode,a.username as username,b.amount as amount from dc_user as a, dc_wallet as b where a.uid=b.uid;
+#修改了一下
+create view dc_user_wallet as select a.uid as uid, a.itcode as itcode,a.username as username,b.amount as amount from dc_user as a, dc_wallet as b where a.uid=b.uid;
+#修改了一下
